cd554edcee16ce5cd87de890193de9df6550e04e
leeyii <lijie@doweidu.com>
2019-12-25 16:34:37 +0800

    Initial Commit

---
+++ TestCodable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestCodable
+//
+//  Created by leeyii on 2019/12/25.
+//  Copyright © 2019 leeyii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
